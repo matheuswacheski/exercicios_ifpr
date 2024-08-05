513519d7f0e8ffc061636530db18aa33f7a9976f
Matheus Wacheski <125933774+matheuswacheski@users.noreply.github.com>
2024-08-05 11:43:15 -0300

    Create fatorial.c

---
+++ fatorial.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <locale.h>
+int main() {
+setlocale (LC_ALL, "Portuguese"); // Permite imprimir acentos e cedilha.
+int n;
+printf("Digite um número inteiro: ");
+scanf("%d", &n);
+fflush(stdin); // Limpa a memória do teclado.
+int i, fat = 1;
+for (i=2; i<=n; i++) {
+fat =fat * i;
+}
+printf("Fatorial é: %d\n", fat);
+}
